423cf400632a1b89c31d9c9a467c2ba1cb2e4e7f
Luis Junco <38411763+luisjunco@users.noreply.github.com>
2025-12-03 12:17:53 +0100

    feat: display movie images

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,17 +5,17 @@ function MovieList() {
         <>
             <h2>List of movies</h2>
 
-            { movies.map( (movieObj) => {
+            {movies.map((movieObj) => {
                 return (
                     <div key={movieObj.id} className="card">
                         <p>{movieObj.title}</p>
+                        <img src={movieObj.imgURL} alt="Movie poster" />
                         <p>Year: {movieObj.year}</p>
                         <p>Rating: {movieObj.rating}</p>
                     </div>
                 )
-            } ) }
+            })}
         </>
-
     )
 }
 
