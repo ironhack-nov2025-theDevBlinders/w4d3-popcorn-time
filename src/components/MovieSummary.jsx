66b6c159b6fd16f932a58841407e627e704b4b4c
Luis Junco <38411763+luisjunco@users.noreply.github.com>
2025-12-04 15:29:34 +0100

    (minor) update message on MovieSummary

--- src/components/MovieSummary.jsx
+++ src/components/MovieSummary.jsx
@@ -12,7 +12,7 @@ function MovieSummary(props) {
             <p>Year: {props.movieDetails.year}</p>
             <p>Rating: {props.movieDetails.rating}</p>
 
-            {/* invoke callback in the parent component */}
+            {/* invoke callback in the grandparent component */}
             <button onClick={() => { props.onDelete(props.movieDetails.id) }}>Delete</button>
 
             <Link to={`/movies/${props.movieDetails.id}`}>
