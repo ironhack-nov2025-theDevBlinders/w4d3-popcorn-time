d6ed7b308eb49cf418c26f2df4a327d16576140f
Luis Junco <38411763+luisjunco@users.noreply.github.com>
2025-12-04 12:19:58 +0100

    refactor: display number of movies in Header (lift state up)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
+import { useState } from "react"
+
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import MovieList from "./components/MovieList"
 
+import movies from "./data/movies.json"
+
 function App() {
+
+  const [moviesToDisplay, setMoviesToDisplay] = useState(movies)
+
+
+  const deleteMovie = (movieId) => {
+    // get the new list of movies...
+    const newList = moviesToDisplay.filter((movieDetails, i, arr) => {
+      if (movieDetails.id !== movieId) {
+        return true;
+      } else {
+        return false;
+      }
+    })
+
+    // update state...
+    // moviesToDisplay = newList; // NEVER, NEVER, MODIFY STATE DIRECTLY!
+    setMoviesToDisplay(newList)
+  }
+
+
   return (
     <>
-      <Header />
-      <MovieList />
+      <Header numberOfMovies={moviesToDisplay.length} />
+      <MovieList moviesArr={moviesToDisplay} onDelete={deleteMovie} />
       <Footer />
     </>
   )
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,39 +1,16 @@
-import { useState } from "react"
-
 import MovieSummary from "./MovieSummary"
-import movies from "../data/movies.json"
-
-function MovieList() {
-
-    const [moviesToDisplay, setMoviesToDisplay] = useState(movies)
 
 
-    const deleteMovie = (movieId) => {
-        // get the new list of movies...
-        const newList = moviesToDisplay.filter((movieDetails, i, arr) => {
-            if (movieDetails.id !== movieId) {
-                return true;
-            } else {
-                return false;
-            }
-        })
-
-        // update state...
-        // moviesToDisplay = newList; // NEVER, NEVER, MODIFY STATE DIRECTLY!
-        setMoviesToDisplay(newList)
-    }
-
+function MovieList(props) {
 
     return (
         <>
-            <h2>Number of movies: {moviesToDisplay.length}</h2>
-
-            {moviesToDisplay.map((movieObj) => {
+            {props.moviesArr.map((movieObj) => {
                 return (
                     <MovieSummary 
                         key={movieObj.id}
                         movieDetails={movieObj}
-                        onDelete={deleteMovie}
+                        onDelete={props.onDelete}
                     />
                 )
             })}
